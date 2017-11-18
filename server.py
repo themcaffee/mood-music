d7f88da83caf0fd87570c14fec63f282977fa8f8
Rahul Ramkumar <rahul.ramkumar97@gmail.com>
2017-11-18 13:52:15 -0700

    refactored the whole thing

--- server.py
+++ server.py
@@ -1,7 +1,16 @@
-import falcon
-import spotipy, json, time, sys, requests, os, re, requests, math
+import spotipy, json, time, sys, requests, os, re, requests, math, logging
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
+
+MAX_MELLOW_DIFFERENCE = 0.15
+NUMBER_OF_RECOMMENDATIONS = 10
+SPOTIFY_PLAYLIST_ID="5Fbeyv4EISgBQF5P1ol9Tt"
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)-16s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 def get_spotify_token():
     client_credentials_manager = SpotifyClientCredentials()
@@ -22,100 +31,56 @@ def get_request_headers():
     headers = {"Authorization" : "Bearer %s" % token}
     return headers
 
-# Falcon follows the REST architectural style, meaning (among
-# other things) that you think in terms of resources and state
-# transitions, which map to HTTP verbs.
-class ThingsResource(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = ('\nTwo things awe me most, the starry sky '
-                     'above me and the moral law within me.\n'
-                     '\n'
-                     '    ~ Immanuel Kant\n\n')
-
-class RecommendationsResource(object):
-    def on_get(self, req, resp):
-        resp.status = falcon.HTTP_200
-        current_song_request = requests.get("https://api.spotify.com/v1/me/player/currently-playing", headers = get_request_headers())
-        current_song_json = json.loads(current_song_request.text)
-        current_song_uri = current_song_json["item"]["uri"].split(':')[2]
-        print(current_song_json["item"]["uri"].split(':')[2])
-
-        payload = {'seed_tracks': current_song_uri}
-        recommendations = requests.get("https://api.spotify.com/v1/recommendations", headers = get_request_headers(), params = payload)
-        recommendations_uri_list = recommendation_attributes(recommendations.text)
-
-        #payload = {'ids': recommendations_uri_list.join(",")}
-        payload = {'ids': ",".join(recommendations_uri_list)}
-        r = requests.get("https://api.spotify.com/v1/audio-features", headers = get_request_headers(), params = payload)
-        resp.body = r.text
-
-class CurrentlyPlayingResource(object):
-    def on_get(self, req, resp):
-        r = requests.get("https://api.spotify.com/v1/me/player/currently-playing", headers = get_request_headers())
-        resp.body = r.text
-
-
 def recommendation_attributes(recommendations_text):
     uris = re.findall(r'\"(?:spotify:track:)([\S]*)\"', recommendations_text)
     print(uris)
     return uris
 
-def send_uris_to_mitch():
+def get_current_song_uri():
     current_song_request = requests.get("https://api.spotify.com/v1/me/player/currently-playing", headers = get_request_headers())
     current_song_json = json.loads(current_song_request.text)
     current_song_uri = current_song_json["item"]["uri"].split(':')[2]
-    print(current_song_json["item"]["uri"].split(':')[2])
+    logger.info("Current Playing Song URI: %s" % current_song_uri)
+    return current_song_uri
 
+def get_mitches_mellow():
     mello = requests.get("http://144.39.201.45:8000/mellow")
-    keep_it_mello = float(mello.text)
-    payload = {'seed_tracks': current_song_uri, 'limit': 10, 'max_energy': keep_it_mello+0.15, 'min_energy': keep_it_mello-0.15}
-    recommendations = requests.get("https://api.spotify.com/v1/recommendations", headers = get_request_headers(), params = payload)
-    recommendations_uri_list = recommendation_attributes(recommendations.text)
+    logger.info("Mitch's mellow is at %s" % mello.text.replace('\n', ''))
+    return float(mello.text)
 
-    #payload = {'ids': recommendations_uri_list.join(",")}
-    payload = {'ids': ",".join(recommendations_uri_list)}
-    r = requests.get("https://api.spotify.com/v1/audio-features", headers = get_request_headers(), params = payload)
-    best_song_id = ""
-    no_r_json = r.json()['audio_features']
-    smallest_difference = 1
-    print(r.json())
-    for i in no_r_json:
+def generate_recommendation():
+    mitches_mellow = get_mitches_mellow()
+    payload = {'seed_tracks': get_current_song_uri(), 
+    'limit': NUMBER_OF_RECOMMENDATIONS, 'max_energy': mitches_mellow+MAX_MELLOW_DIFFERENCE, 
+    'min_energy': mitches_mellow-MAX_MELLOW_DIFFERENCE}
+    r = requests.get("https://api.spotify.com/v1/audio-features", 
+        headers = get_request_headers(), params = payload)
+    json_data = r.json()['audio_features']
+    smallest_diff = 1
+    best_song_id = ''
+    for i in json_data:
         energy = i['energy']
-        diff = math.fabs(energy-(1-keep_it_mello))
-        if diff < smallest_difference:
+        diff = math.fabs(energy-(1-mitches_mellow))
+        if diff < smallest_diff:
             best_song_id = i["id"]
-            smallest_difference = diff
-    #mitch = requests.post("http://144.39.201.45:8000/predict", json=json.loads(r.text))
-    print(best_song_id)
-    track_uri = "spotify:track:%s" % best_song_id.replace("\"", '')
+            smallest_diff = diff
+    logger.info("Closest Song URI: %s" % best_song_id)
+    return best_song_id
+
+def append_song_to_playlist():
+    track_uri = "spotify:track:%s" % generate_recommendation().replace("\"", '')
     track_uri = track_uri.replace('\n', '')
     headers = get_request_headers()
     headers['Content-Type'] = 'application/json'
     data_var = "{\"uris\":[\"%s\"]}" % track_uri
-    r = requests.post("https://api.spotify.com/v1/users/%s/playlists/%s/tracks" % (get_spotify_user_id(), "5Fbeyv4EISgBQF5P1ol9Tt"), headers = headers, data=data_var)
-    print(data_var)
-    q = requests.get("https://api.spotify.com/v1/audio-features/?ids=%s" % (",".join([track_uri.split(":")[2], current_song_uri])), headers = get_request_headers())
-    print((",".join([track_uri, current_song_uri])))
+    r = requests.post("https://api.spotify.com/v1/users/%s/playlists/%s/tracks" % (get_spotify_user_id(), SPOTIFY_PLAYLIST_ID), headers = headers, data=data_var)
+    logger.info("Added track to playlist: %s" % track_uri)
+    q = requests.get("https://api.spotify.com/v1/audio-features/?ids=%s" % (",".join([track_uri.split(":")[2], get_current_song_uri()])), headers = get_request_headers())
     print(q.text)
 
-
-# falcon.API instances are callable WSGI apps
-app = falcon.API()
-
-# Resources are represented by long-lived class instances
-things = ThingsResource()
-recommendations = RecommendationsResource()
-currently_playing = CurrentlyPlayingResource()
-
-# things will handle all requests to the '/things' URL path
-app.add_route('/things', things)
-app.add_route('/recommendations', recommendations)
-app.add_route('/currently-playing', currently_playing)
-
 def main():
-    send_uris_to_mitch()
+    append_song_to_playlist()
+    
 
 if __name__ == '__main__':
     main()
